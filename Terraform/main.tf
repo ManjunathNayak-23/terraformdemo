6cd06beb4edbea599ba86a31c0b0ede9a8ff5db3
Manjunath Nayak <65599992+ManjunathNayak-23@users.noreply.github.com>
2023-04-18 18:35:05 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,16 +1,20 @@
-terraform { 
-  backend "azurerm" {} 
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
   }
+    backend "azurerm" {} 
+}
 
-# Configure the Azure Provider
+# Configure the Microsoft Azure Provider
 provider "azurerm" {
-  # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
-  version = "=2.20.0"
   features {}
 }
 
 # Create a resource group
 resource "azurerm_resource_group" "example" {
   name     = "example-resources"
-  location = "East US"
+  location = "West Europe"
 }
